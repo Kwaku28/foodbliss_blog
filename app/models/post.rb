00f0f3e98e4f18e99ea43f065c85b7a10c46d5ed
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-13 07:10:53 +0000

    Fix linters issues

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   # A post belongs to an author
   # A post has many comments
   # A post has many likes
-  belongs_to :author, class_name: 'User' foreign_key: :author_id
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
 end
