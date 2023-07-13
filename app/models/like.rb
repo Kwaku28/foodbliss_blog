af033d9b3f86c098ad23f3fe1be45b74b783a171
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-13 06:47:05 +0000

    Add associations to the model files

---
+++ app/models/like.rb
@@ -0,0 +1,7 @@
+class Like < ApplicationRecord
+  # Associations
+  # A like belongs to an author
+  # A like belongs to a post
+  belongs_to :author class_name: 'User' foreign_key: :author_id
+  belongs_to :post
+end
