6ddcc683c30df26f07cca289c890cdb66ee48717
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-13 06:46:15 +0000

    Add attributes to the migration files generated

---
+++ db/migrate/20230713050201_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :Title
+      t.text :Text
+      t.integer :CommentsCounter
+      t.integer :LikesCounter
+      t.references :author, null: false, foreign_key: { to_table: 'users' }, index: true
+
+      t.timestamps
+    end
+  end
+end
