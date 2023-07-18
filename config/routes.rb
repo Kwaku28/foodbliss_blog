94c2a062d552bdd22c04cce98909538d76a29fc7
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-18 12:09:13 +0000

    Add routes to handle the required URLs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "users#index"
+  root 'users#index'
+
+  get '/users', to: 'users#index'
+  get '/users/:id', to: 'users#show'
+  get '/users/:author_id/posts', to: 'posts#index'
+  get '/users/:author_id/posts/:id', to: 'posts#show'
+
 end
