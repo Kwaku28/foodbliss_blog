ac85563929e540e349b8643a1644307c23a25514
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-21 12:49:01 +0000

    Add method to handle counters

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,4 +4,11 @@ class Like < ApplicationRecord
   # A like belongs to a post
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :post
+
+  before_save :update_likes_counter
+
+  # Updates the likes counter for a post
+  def update_likes_counter
+    Post.find_by(id: post_id).increment!(:likes_counter)
+  end
 end
