b41373e285c2596c65c66c51e6bcaa1e9ea4021e
Lawrence Amoafo <rekenburg777@gmail.com>
2023-07-22 02:01:53 +0000

    Add method to controller to handle creating a comment

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      flash[:success] = 'Comment saved successfully'
+      redirect_to users_path
+    else
+      flash.now[:error] = 'Error: Comment could not be saved'
+      render :new
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
+end
