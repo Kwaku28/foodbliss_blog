2d750500d2a74cb524f0d9429cf56586284a94e3
Joy Phoebe <joyapisi@gmail.com>
2023-07-25 16:41:11 +0300

    Add capybara tests in features/post_spec

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -47,4 +47,33 @@ RSpec.describe 'User', type: :feature do
       expect(page).to have_content 'post 1 title by Lawrence Amoafo'
     end
   end
+  describe 'post show' do
+    before(:each) do
+      visit "/users/#{@user1.id}/posts/#{@post1.id}"
+    end
+    it 'I can see the posts title.' do
+      expect(page).to have_content 'post 1 title'
+    end
+    it 'I can see who wrote the post.' do
+      expect(page).to have_content 'Lawrence Amoafo'
+      expect(page).to have_content 'post 1 title by Lawrence Amoafo'
+    end
+    it 'I can see how many comments it has.' do
+      expect(page).to have_content 'Comments: 1'
+      expect(page).to_not have_content 'Comments: 0'
+    end
+    it 'I can see how many likes it has.' do
+      expect(page).to have_content 'Likes: 0'
+      expect(page).to_not have_content 'Likes: 1'
+    end
+    it 'I can see the post body.' do
+      expect(page).to have_content 'post content 1'
+    end
+    it 'I can see the username of each commentor.' do
+      expect(page).to have_content @user1.name.to_s
+    end
+    it 'I can see the comment each commentor left.' do
+      expect(page).to have_content 'First comment from Lawrence'
+    end
+  end
 end
